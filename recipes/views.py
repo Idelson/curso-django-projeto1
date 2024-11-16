b803094254fa96b048195cbb25edec81c4a62ad7
Idelson <idelsonjr@hotmail.com>
2024-11-16 11:30:14 -0300

    Inclusão projeto Recipes

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Idelson Júnior'
+    })
+
+def contato(request):
+    return HttpResponse('contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
